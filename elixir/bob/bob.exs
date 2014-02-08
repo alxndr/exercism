6a349cf31606f2be905f499a142c72a3d7d4ae30
Alexander <alxndr+github@gmail.com>
2014-02-07 22:12:42 -0800

    elixir: bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,6 +1,14 @@
 defmodule Teenager do
 
-  def hey do
+  def hey(input) do
+    cond do
+      Regex.match?(%r/[[:alpha:]]/, input) and String.upcase(input) == input -> "Woah, chill out!"
+      Regex.match?(%r/\?$/, input) -> "Sure."
+      Regex.match?(%r/^\s*$/, input) -> "Fine. Be that way!"
+      true -> "Whatever."
+    end
   end
+
+  # defp # private function
 
 end
