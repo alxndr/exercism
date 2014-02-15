58e6d1cd7b761a28cf73806a71eb75b09f5e68b3
Alexander <alxndr+github@gmail.com>
2014-02-15 10:23:47 -0800

    js: beer-song: streamlining

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,7 +1,3 @@
-function pluralize(count, noun) {
-  return noun + (count == 1 ? '' : 's');
-}
-
 function bottles_phrase(count) {
   if (!count) {
     count = 'no more';
@@ -12,14 +8,10 @@ function bottles_phrase(count) {
 function take_action(count) {
   var results = {};
   if (count) {
-    if (count == 1) {
-      results.action_phrase = 'Take it down and pass it around';
-    } else {
-      results.action_phrase = 'Take one down and pass it around';
-    }
+    results.phrase = 'Take ' + (count == 1 ? 'it' : 'one') + ' down and pass it around';
     results.new_count = count - 1;
   } else {
-    results.action_phrase = 'Go to the store and buy some more';
+    results.phrase = 'Go to the store and buy some more';
     results.new_count = 99;
   }
   return results;
@@ -29,29 +21,34 @@ function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function pluralize(count, noun) {
+  return noun + (count == 1 ? '' : 's');
+}
+
 
 var song = {
 
   verse : function verse(count) {
+    var action;
     if (count < 0) {
       throw new Error("can't have fewer than 0 bottles!'");
     }
-    var output = [];
-    var action = take_action(count);
-    output.push(capitalize(bottles_phrase(count)) + ' on the wall, ');
-    output.push(bottles_phrase(count) + '.\n');
-    output.push(action.action_phrase + ', ' + bottles_phrase(action.new_count) + ' on the wall.\n');
-    return output.join('');
+    action = take_action(count);
+    return capitalize(bottles_phrase(count)) + ' on the wall, ' +
+      bottles_phrase(count) + '.\n' +
+      action.phrase + ', ' +
+      bottles_phrase(action.new_count) + ' on the wall.\n';
   },
 
   sing : function sing(start, end) {
+    var output;
     if (end === undefined) {
       return this.sing(start, 0);
     }
     if (start < end) {
       throw new Error("that's dangerous thinking there");
     }
-    var output = [];
+    output = [];
     for (var i = start; i >= end; i--) {
       output.push(this.verse(i));
     }
