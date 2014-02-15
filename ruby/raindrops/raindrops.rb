3a7fe13333a618203081261174aa673d91d4fd80
Alexander <alxndr+github@gmail.com>
2014-02-14 17:34:01 -0800

    ruby: raindrops: splitting out a new class

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,26 +1,23 @@
 class Raindrops
-
   def self.convert(num)
-    factors = Raindrops.find_factors(num)
+    factors = FactorFinder.find_factors(num)
+
     output = ''
-    if factors.include?(3) || num == 3
-      output += 'Pling'
-    end
-    if factors.include?(5) || num == 5
-      output += 'Plang'
-    end
-    if factors.include?(7) || num == 7
-      output += 'Plong'
-    end
+    output += 'Pling' if factors.include?(3) || num == 3
+    output += 'Plang' if factors.include?(5) || num == 5
+    output += 'Plong' if factors.include?(7) || num == 7
+
     if output.empty?
       num.to_s
     else
       output
     end
   end
+end
 
+class FactorFinder
   def self.find_factors(num)
+    # returns factors excluding 1 and itself
     (2..(num/2)).select { |i| (num % i).zero? }
   end
-
 end
