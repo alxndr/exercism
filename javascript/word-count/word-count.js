a6d0484d46c848f4efc10a2e9ed7a480c42b6cf4
Alexander <alxndr+github@gmail.com>
2014-02-15 18:47:48 -0800

    Revert "js: word-count: WIP"
    This reverts commit 2a74e50bc57ae5c72060c3e2aab66ef173e81c55.

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,18 +1,6 @@
 function word_analyzer(text) {
   var analysis, corpus;
-  function scan(str, regex) {
-    if (!regex.global) {
-      throw "regex must have 'global' flag set";
-    }
-    var r = [];
-    str.replace(regex, function() {
-      r.push(Array.prototype.slice.call(arguments, 1, -2));
-    });
-    return r;
-  }
   function strip_punctuation(str) {
-    var scanned = scan(str, /([a-z0-9 ]+)/g);
-    console.log(scanned[0]);
     return str.replace(/[^a-z0-9 ]/g, '');
   }
   function analyze(accumulator, element) {
