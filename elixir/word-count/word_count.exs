4e29ed552db717fcb245f42934fc7bbe88fa5fb2
Alexander <alxndr+github@gmail.com>
2014-02-15 18:15:05 -0800

    elixir: word-count: remove comment

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -2,7 +2,7 @@ defmodule Words do
 
   def count(text) do
     increment = fn(x)-> x + 1 end
-    Regex.replace(%r/[^a-z0-9 ]/i, String.downcase(text), '') # why need parens?
+    Regex.replace(%r/[^a-z0-9 ]/i, String.downcase(text), '')
     |> String.split
     |> Enum.reduce HashDict.new, fn(word, acc)->
       HashDict.update acc, word, 1, increment
