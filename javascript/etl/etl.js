b0eafaa52d2540f313daaea4436856eb71c37462
Alexander <alxndr+github@gmail.com>
2014-02-16 18:41:45 -0800

    js: etl: playing with closures

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,10 +1,15 @@
 function transform(old_schema) {
   var new_schema = {};
+
+  function add_value_to_new_key(schema, new_val) {
+    return function add_something(old_value) {
+      schema[old_value.toLowerCase()] = parseInt(new_val, 10);
+    };
+  }
   Object.keys(old_schema).forEach(function(old_key) {
-    old_schema[old_key].forEach(function(old_value) {
-      new_schema[old_value.toLowerCase()] = parseInt(old_key, 10);
-    });
+    old_schema[old_key].forEach(add_value_to_new_key(new_schema, old_key));
   });
+
   return new_schema;
 }
 
