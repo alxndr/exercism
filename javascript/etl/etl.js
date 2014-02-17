b512bcecf777fb1c05ec3ddc79fe16fcb10e48ce
Alexander <alxndr+github@gmail.com>
2014-02-16 18:59:55 -0800

    js: etl: Array#reduce

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,16 +1,17 @@
+function transformer(old_schema) {
+  return function reducer(new_schema, points) {
+    var words = old_schema[points];
+
+    words.forEach(function value_into_new_schema(word) {
+      new_schema[word.toLowerCase()] = parseInt(points, 10);
+    });
+
+    return new_schema;
+  };
+}
+
 function transform(old_schema) {
-  var new_schema = {};
-
-  function add_value_to_new_key(schema, new_val) {
-    return function add_something(old_value) {
-      schema[old_value.toLowerCase()] = parseInt(new_val, 10);
-    };
-  }
-  Object.keys(old_schema).forEach(function(old_key) {
-    old_schema[old_key].forEach(add_value_to_new_key(new_schema, old_key));
-  });
-
-  return new_schema;
+  return Object.keys(old_schema).reduce(transformer(old_schema), {});
 }
 
 if (module) {
