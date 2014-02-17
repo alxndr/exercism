1e92d1ebd0b26ff133c6e9b9972248e9c6cf6204
Alexander <alxndr+github@gmail.com>
2014-02-16 17:50:08 -0800

    ruby: proverb: remove some instance vars

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -1,23 +1,23 @@
 class Proverb
 
   def initialize(*args, **opts)
-    @nouns = args
-    @opts = opts
-    generate_lines
+    opts ||= {}
+    @proverb_lines = generate_lines(args, opts[:qualifier])
   end
 
   def to_s
-    @proverb.join "\n"
+    @proverb_lines.join "\n"
   end
 
   private
 
-  def generate_lines
-    noun_pairs = @nouns.zip(@nouns[1..-1])[0..-2] # [[nail, shoe], [shoe, horse], ...]
-    qualifier = @opts[:qualifier] ? @opts[:qualifier] + ' ' : ''
+  def generate_lines(nouns, qualifier)
+    noun_pairs = nouns.zip(nouns[1..-1])[0..-2] # [[nail, shoe], [shoe, horse], ..., [battle, kingdom]
+    puts noun_pairs.inspect
+    qualifier = qualifier ? qualifier + ' ' : ''
 
-    @proverb = noun_pairs.map { |first, second| "For want of a #{first} the #{second} was lost." }
-    @proverb.push "And all for the want of a #{qualifier}#{@nouns.first}."
+    lines = noun_pairs.map { |first, second| "For want of a #{first} the #{second} was lost." }
+    lines.push "And all for the want of a #{qualifier}#{nouns.first}."
   end
 
 end
