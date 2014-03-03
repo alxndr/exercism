a6739a1429a86b4f28eef0c92f293138f3080d6d
Alexander <alxndr+github@gmail.com>
2014-03-03 09:30:23 -0800

    js: anagram: pull private methods out of constructor

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,19 +1,19 @@
+function alphabetize(letters) {
+  return letters.toLowerCase().split('').sort().join('');
+}
+
+function is_identical(a, b) {
+  return (a.toLowerCase() == b.toLowerCase());
+}
+
 function Anagram(word) {
   var letters_to_match = alphabetize(word);
 
   this.match = function(potential_anagrams) {
     return potential_anagrams.filter(function(potential_match) {
-      return (!is_identical(potential_match) && alphabetize(potential_match) == letters_to_match);
+      return (!is_identical(word, potential_match) && alphabetize(potential_match) == letters_to_match);
     });
   };
-
-  function alphabetize(t) {
-    return t.toLowerCase().split('').sort().join('');
-  }
-  function is_identical(t) {
-    return (t.toLowerCase() == word.toLowerCase());
-  }
-
 }
 
 if (module) {
