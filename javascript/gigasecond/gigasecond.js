b0bbe94bfd7e8ee1a4a045c760c49f989b4c6d42
Alexander <alxndr+github@gmail.com>
2014-03-03 10:44:55 -0800

    js: gigasecond: double new Date() to avoid having to .setX(0)

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,11 +1,7 @@
-var something = +'1e12';
+var giga = +'1e12';
 function Gigasecond(start_date) {
   function gigasecond_date() {
-    var in_the_future = new Date(+start_date + something);
-    in_the_future = new Date(in_the_future.setHours(0));
-    in_the_future = new Date(in_the_future.setMinutes(0));
-    in_the_future = new Date(in_the_future.setSeconds(0));
-    return in_the_future;
+    return new Date(new Date(+start_date + giga).toDateString());
   }
 
   this.date = gigasecond_date;
