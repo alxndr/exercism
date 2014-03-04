641b8450fbe90bffc17f7fa86297a829dbfb0f4b
Alexander <alxndr+github@gmail.com>
2014-03-03 19:31:33 -0800

    js: phone-number: attack of the higher order functions

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,36 +1,54 @@
-function PhoneNumber(dirty_number) {
-  var cleaned_number;
-  function clean(a_number) {
-    var cleaned = remove_non_numerals(a_number);
-    if (cleaned.length == 11 && cleaned.substr(0, 2) === '11') {
-      cleaned = cleaned.slice(1);
-    }
-    return cleaned;
-  }
-  function remove_non_numerals(string) {
-    return string.replace(/[^0-9]/g,'');
-  }
-  function is_valid(clean_number) {
-    return (clean_number.length == 10);
-  }
-  function number() {
-    return is_valid(cleaned_number) ? cleaned_number : '0000000000';
-  }
-  function area_code() {
-    return cleaned_number.substr(0, 3);
-  }
-  function to_string() {
-    return '(' + area_code() + ') ' +
-      cleaned_number.substr(3, 3) +
-      '-' +
-      cleaned_number.substr(6, 4);
-  }
-  cleaned_number = clean(dirty_number);
+function remove_non_numerals(string) {
+  return string.replace(/[^0-9]/g,'');
+}
 
-  // public interface
-  this.number = number;
-  this.areaCode = area_code;
-  this.toString = to_string;
+function is_ten_long(clean_number) {
+  return (clean_number.length == 10);
+}
+
+function has_extra_one(num) {
+  return (num.length == 11 && num.substr(0, 2) === '11');
+}
+
+function clean(a_number) {
+  var cleaned = remove_non_numerals(a_number);
+  if (has_extra_one(cleaned)) {
+    cleaned = cleaned.slice(1);
+  }
+  return cleaned;
+}
+
+function first_three_digits(num) {
+  return num.substr(0, 3);
+}
+
+function make_number_getter(num) {
+  var val = is_ten_long(num) ? num : '0000000000';
+  return function number() {
+    return val;
+  };
+}
+
+function make_area_code_getter(num) {
+  var val = first_three_digits(num);
+  return function area_code() {
+    return val;
+  };
+}
+
+function make_to_string_getter(num) {
+  var val = '(' + first_three_digits(num) + ') ' + num.substr(3, 3) + '-' + num.substr(6, 4);
+  return function to_string() {
+    return val;
+  };
+}
+
+function PhoneNumber(dirty_number) {
+  var cleaned_number = clean(dirty_number);
+
+  this.number = make_number_getter(cleaned_number);
+  this.areaCode = make_area_code_getter(cleaned_number);
+  this.toString = make_to_string_getter(cleaned_number);
 }
 
 if (module) {
