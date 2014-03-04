b25003721743783beb13aab0c6e57913223ac62c
Alexander <alxndr+github@gmail.com>
2014-03-03 16:00:52 -0800

    ruby: roman-numerals: one method to convert them all
    also rename some things

--- ruby/roman-numerals/roman.rb
+++ ruby/roman-numerals/roman.rb
@@ -9,28 +9,26 @@ module RomanNumerable
      500 => 'D',
     1000 => 'M'
   }
-  BASES = [1000, 100, 10, 1]
 
   def to_roman
-    BASES.reduce('') do |in_roman, place|
-      in_roman += send("convert_place_#{place}", (self % (place * 10)) / place)
+    [1000, 100, 10, 1].reduce('') do |in_roman, place|
+      in_roman += convert_place(place, (self % (place * 10)) / place)
     end
   end
 
-  BASES.each do |place|
-    define_method("convert_place_#{place}") do |x|
-      case x
-      when 0, 1, 2, 3
-        place.roman_chr * x
-      when 4
-        place.roman_chr + (place * 5).roman_chr
-      when 5, 6, 7, 8
-        (place * 5).roman_chr + (place.roman_chr * (x - 5))
-      when 9
-        place.roman_chr + (place * 10).roman_chr
-      else
-        raise ArgumentError, 'argument is not an integer such that: 0 <= n <= 9'
-      end
+  def convert_place(order_of_magnitude, n)
+    raise ArgumentError, "order_of_magnitude does not have a Roman numeral equivalent: #{order_of_magnitude.inspect}" unless ROMAN_NUMERABLE_MAPPING.has_key?(order_of_magnitude)
+    case n
+    when 0, 1, 2, 3
+      order_of_magnitude.roman_chr * n
+    when 4
+      order_of_magnitude.roman_chr + (order_of_magnitude * 5).roman_chr
+    when 5, 6, 7, 8
+      (order_of_magnitude * 5).roman_chr + (order_of_magnitude.roman_chr * (n - 5))
+    when 9
+      order_of_magnitude.roman_chr + (order_of_magnitude * 10).roman_chr
+    else
+      raise ArgumentError, "n is not an integer such that: 0 <= n <= 9: #{n.inspect}"
     end
   end
 
