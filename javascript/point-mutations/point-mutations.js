6ab57033e300dfeb25142dc0b057b25e3442aeb1
Alexander <alxndr+github@gmail.com>
2014-03-03 21:11:13 -0800

    js: point-mutations: make_filter(list)
    also remove the conditional around trimming other_sequence

--- javascript/point-mutations/point-mutations.js
+++ javascript/point-mutations/point-mutations.js
@@ -1,17 +1,16 @@
+function make_filter(list) {
+  return function does_not_match_list(element, i) {
+    return list[i] != element;
+  };
+}
+
 function DNA(sequence) {
   var dna_sequence = sequence.split('');
 
-  function does_not_match_sequence(nucleotide, i) {
-    return dna_sequence[i] != nucleotide;
-  }
   function hamming_distance(other_sequence) {
-    if (other_sequence.length > dna_sequence.length) {
-      other_sequence = other_sequence.substr(0, dna_sequence.length);
-    }
-    return other_sequence.split('').filter(does_not_match_sequence).length;
+    return other_sequence.split('').splice(0, dna_sequence.length).filter(make_filter(dna_sequence)).length;
   }
 
-  // public interface
   this.hammingDistance = hamming_distance;
 }
 
