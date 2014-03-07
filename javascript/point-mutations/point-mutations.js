cb42e7eb423ef59312cd3684aac404062282d55a
Alexander <alxndr+github@gmail.com>
2014-03-07 09:45:11 -0800

    js: point-mutations: get rid of make_filter, put matching function back into DNA constructor

--- javascript/point-mutations/point-mutations.js
+++ javascript/point-mutations/point-mutations.js
@@ -1,17 +1,23 @@
-function make_filter(list) {
-  return function does_not_match_list(element, i) {
-    return list[i] != element;
-  };
-}
-
 function DNA(sequence) {
   var dna_sequence = sequence.split('');
 
-  function hamming_distance(other_sequence) {
-    return other_sequence.split('').splice(0, dna_sequence.length).filter(make_filter(dna_sequence)).length;
+  function nucleotide_at(index) {
+    return dna_sequence[index];
   }
 
-  this.hammingDistance = hamming_distance;
+  function does_not_match_sequence(other_nucleotide, index) {
+    return nucleotide_at(index) != other_nucleotide;
+  }
+
+  function find_hamming_distance_to(other_sequence) {
+    return other_sequence.
+      split('').
+      splice(0, dna_sequence.length).
+      filter(does_not_match_sequence).
+      length;
+  }
+
+  this.hammingDistance = find_hamming_distance_to;
 }
 
 if (module) {
