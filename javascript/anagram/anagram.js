bab1ba5aaa1201addf2386031d47e06e29fbbf17
Alexander <alxndr+github@gmail.com>
2014-03-06 20:13:01 -0800

    js: anagram: move anagrammy things back into constructor

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,23 +1,21 @@
-function alphabetize(letters) {
-  return letters.toLowerCase().split('').sort().join('');
-}
-
-function find_anagrams_of(first_word) {
-  first_word = first_word.toLowerCase();
-  return function keep_matches(second_word) {
-    second_word = second_word.toLowerCase();
-    return (alphabetize(first_word) === alphabetize(second_word) && first_word !== second_word);
-  };
-}
-
-function Anagram(first_word) {
-  function match(potential_anagrams) {
-    return potential_anagrams.filter(find_anagrams_of(first_word));
+function Anagram(starting_word) {
+  function is_an_anagram(other_word) {
+    other_word = other_word.toLowerCase();
+    return (starting_word !== other_word && starting_word_alphabetized === other_word.alphabetize());
   }
+  function find_matches_among(potential_anagrams) {
+    return potential_anagrams.filter(is_an_anagram);
+  }
+  starting_word = starting_word.toLowerCase();
+  starting_word_alphabetized = starting_word.alphabetize();
 
-  this.match = match;
+  this.match = find_matches_among;
 }
 
 if (module) {
   module.exports = Anagram;
 }
+
+String.prototype.alphabetize = function String_alphabetize() {
+  return this.toLowerCase().split('').sort().join('');
+};
