640078802c2bf9857002144c13fce6e5d802d915
Alexander <alxndr+github@gmail.com>
2014-03-07 09:22:33 -0800

    js: leap: encapusale all those comparisons in a Year object

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,7 +1,23 @@
+function Year(year) {
+  function is_divisible_by_four() {
+    return year % 4 === 0;
+  }
+  function is_not_century() {
+    return year % 100 !== 0;
+  }
+  function is_quad_century() {
+    return year % 400 === 0;
+  }
+
+  this.is_leap_year = function() {
+    return is_divisible_by_four() &&
+      is_not_century() ||
+      is_quad_century();
+  };
+}
+
 function is_leap_year(year) {
-  return (year % 4 === 0 &&
-          year % 100 !== 0 ||
-          year % 400 === 0);
+  return new Year(year).is_leap_year();
 }
 
 if (module) {
