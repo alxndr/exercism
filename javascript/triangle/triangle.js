d0971cab260ebc2ae40a70722ac53ec8c5e1bfc9
Alexander <alxndr+github@gmail.com>
2014-03-15 15:11:37 -0700

    js: triangle: make Triangle#kind into a getter
    determine triangle kind in the constructor. little weird how the test
    expects the Error throw so it's in the exposed method instead of the
    constructor.
    
    still don't know if polymorphism in javascript is worth it enough to get
    rid of that switch block.

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,21 +1,31 @@
 function Triangle(a, b, c) {
+  var is_missing_number = (a === undefined || b === undefined || c === undefined || a === 0 || b === 0 || c === 0);
+  var is_impossible_triangle = (a < 0 || b < 0 || c < 0 || a + b <= c || b + c <= a || c + a <= b);
+  var is_equilateral = (a === b && b === c);
+  var is_isoceles = (a === b || b === c || c === a);
+  var triangle_kind = determine_kind();
+
   function determine_kind() {
-    if (a === undefined || b === undefined || c === undefined || a === 0 || b === 0 || c === 0) {
+    switch (true) {
+      case is_missing_number:
+      case is_impossible_triangle:
+        return 'illegal';
+      case is_equilateral:
+        return 'equilateral';
+      case is_isoceles:
+        return 'isosceles';
+      default:
+        return 'scalene';
+    }
+  }
+  function return_triangle_kind() {
+    if (is_missing_number) {
       throw new Error('that does not seem like a triangle');
     }
-    if (a < 0 || b < 0 || c < 0 || a + b <= c || b + c <= a || c + a <= b) {
-      return 'illegal';
-    }
-    if (a === b && b === c) {
-      return 'equilateral';
-    }
-    if (a === b || b === c || c === a) {
-      return 'isosceles';
-    }
-    return 'scalene';
+    return triangle_kind;
   }
 
-  this.kind = determine_kind;
+  this.kind = return_triangle_kind;
 }
 
 if (module) {
