6de187285786c4d64ac5cc61923f08063e8468f1
Alexander <alxndr+github@gmail.com>
2014-03-18 19:10:32 -0700

    js: etl: purty things up a little

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,12 +1,20 @@
-function transform(old_schema) {
-  return Object.keys(old_schema).reduce(function(new_schema,point_value){
-    return old_schema[point_value].reduce(function(ns, word){
-      ns[word.toLowerCase()] = parseInt(point_value, 10);
-      return ns;
-    }, new_schema);
-  }, {});
-}
-
 if (module) {
   module.exports = transform;
+}
+
+function transform(old_schema) {
+  return Object.keys(old_schema).reduce(transformer(old_schema), {});
+}
+
+function transformer(old_schema) {
+  return function(new_schema_accumulator, point_value) {
+    return old_schema[point_value].reduce(function(new_schema, word){
+      new_schema[word.toLowerCase()] = int(point_value);
+      return new_schema;
+    }, new_schema_accumulator);
+  };
+}
+
+function int(a_string) {
+  return parseInt(a_string, 10);
 }
