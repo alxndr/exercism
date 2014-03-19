39ac45373919728859ccd6e01af3d0b44f90cca1
Alexander <alxndr+github@gmail.com>
2014-03-18 21:32:06 -0700

    elixir: bob: local methods. not passing the russian test though

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,14 +1,25 @@
 defmodule Teenager do
 
   def hey(input) do
+    trimmed = input |> String.strip
     cond do
-      Regex.match?(%r/[[:alpha:]]/, input) and String.upcase(input) == input -> "Woah, chill out!"
-      Regex.match?(%r/\?$/, input) -> "Sure."
-      Regex.match?(%r/^\s*$/, input) -> "Fine. Be that way!"
+      trimmed |> is_empty?      -> "Fine. Be that way!"
+      trimmed |> is_yelling?    -> "Woah, chill out!"
+      trimmed |> is_a_question? -> "Sure."
       true -> "Whatever."
     end
   end
 
-  # defp # private function
+  defp is_empty?(string) do
+    string == ""
+  end
+
+  defp is_yelling?(string) do
+    string |> String.upcase == string and string |> String.match? %r/[[:alpha:]]/
+  end
+
+  defp is_a_question?(string) do
+    string |> String.ends_with?("?")
+  end
 
 end
