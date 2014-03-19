94d202c245b2e3f371eb60132df7df4c737793e6
Alexander <alxndr+github@gmail.com>
2014-03-18 19:16:06 -0700

    js: etl: try to clarify new schema variable

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -7,11 +7,11 @@ function transform(old_schema) {
 }
 
 function transformer(old_schema) {
-  return function(new_schema_accumulator, point_value) {
-    return old_schema[point_value].reduce(function(new_schema, word){
+  return function(new_schema, point_value) {
+    return old_schema[point_value].reduce(function(_, word){
       new_schema[word.toLowerCase()] = int(point_value);
       return new_schema;
-    }, new_schema_accumulator);
+    }, new_schema);
   };
 }
 
