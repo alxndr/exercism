e19f9a965a3059eee7e36103eace4c87f272f8f6
Alexander <alxndr+github@gmail.com>
2014-03-19 14:03:25 -0700

    elixir: word-count: List.flatten before Enum.reduce

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -2,13 +2,12 @@ defmodule Words do
 
   @spec count(String.t) :: HashDict.t
   def count(text) do
-    %r/[\p{L}0-9]+/i |> Regex.scan(String.downcase(text)) |> count_words
-  end
-
-  defp count_words(lists) do
-    increment = fn(x)-> x + 1 end
-    lists |> Enum.reduce HashDict.new, fn(list_of_one_word, acc)->
-      HashDict.update acc, List.first(list_of_one_word), 1, increment
+    increment = fn(x)-> x + 1 end # TODO syntax for anonymous fns
+    %r/[\p{L}0-9]+/i
+    |> Regex.scan(String.downcase(text))
+    |> List.flatten
+    |> Enum.reduce HashDict.new, fn(word, acc)->
+      HashDict.update acc, word, 1, increment
     end
   end
 
