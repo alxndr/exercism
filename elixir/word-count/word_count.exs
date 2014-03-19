d7e7c1e1912e5ab82d4bdc6d7e2502a573d0f24f
Alexander <alxndr+github@gmail.com>
2014-03-19 10:15:56 -0700

    elixir: word-count: split out a private method

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -1,11 +1,13 @@
 defmodule Words do
 
   def count(text) do
+    %r/[a-z0-9]+/i |> Regex.scan(String.downcase(text)) |> count_words
+  end
+
+  defp count_words(lists) do
     increment = fn(x)-> x + 1 end
-    Regex.replace(%r/[^a-z0-9 ]/i, String.downcase(text), '')
-    |> String.split
-    |> Enum.reduce HashDict.new, fn(word, acc)->
-      HashDict.update acc, word, 1, increment
+    lists |> Enum.reduce HashDict.new, fn(list_of_one_word, acc)->
+      HashDict.update acc, List.first(list_of_one_word), 1, increment
     end
   end
 
