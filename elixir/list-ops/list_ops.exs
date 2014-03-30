cf3a4358c74ed579fa844fd2b688955cb47601df
Alexander <alxndr+github@gmail.com>
2014-03-30 10:25:41 -0700

    elixir: list-ops: use [ | ] where possible, including reverse/1

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -1,22 +1,23 @@
 defmodule ListOps do
 
   @spec count(list) :: non_neg_integer
-  def count([]),           do: 0
-  def count([_head|tail]), do: 1 + count(tail)
+  def count([]),         do: 0
+  def count([ _h | t ]), do: 1 + count(t)
 
   @spec reverse(list) :: list
-  def reverse([]),          do: []
-  def reverse([head|tail]), do: :lists.append(reverse(tail), [head])
+  def reverse(list),             do: reverse(list, [])
+  def reverse([], rlist),        do: rlist
+  def reverse([ h | t ], rlist), do: reverse(t, [h | rlist])
 
   @spec map(list, (any -> any)) :: list
-  def map([], _f),         do: []
-  def map([head|tail], f), do: :lists.append([f.(head)], map(tail, f))
+  def map([], _f),             do: []
+  def map([ head | tail ], f), do: [ f.(head) | map(tail, f) ]
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter([], _f),         do: []
-  def filter([head|tail], f) do
+  def filter([], _f),            do: []
+  def filter([ head | tail ], f) do
     if f.(head) do
-      :lists.append([ head ], filter(tail, f))
+      [ head | filter(tail, f) ]
     else
       filter(tail, f)
     end
@@ -32,8 +33,8 @@ defmodule ListOps do
   @spec append(list, list) :: list
   def append([], list),         do: list
   def append(list, []),         do: list
-  def append(list, [head|tail]) do
-    append(:lists.append(list, [head]), tail)
+  def append(list, list2) do
+    :lists.append list, list2
   end
 
   @spec concat([[any]]) :: [any]
