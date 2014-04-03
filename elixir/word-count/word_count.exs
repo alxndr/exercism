1b15852abb44f8856b09e1a6c3e6c0488ab7c90b
Alexander <alxndr+github@gmail.com>
2014-04-03 09:39:25 -0700

    elixir: word-count: shake things out a little

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -1,14 +1,18 @@
 defmodule Words do
 
   def count(text) do
-    %r/[\p{L}0-9]+/i |> Regex.scan(String.downcase(text)) |> count_words
+    text |>
+    split_words |>
+    count_words
   end
 
-  defp count_words(lists) do
-    increment = fn(x)-> x + 1 end
-    lists |> Enum.reduce HashDict.new, fn(list_of_one_word, acc)->
-      HashDict.update acc, List.first(list_of_one_word), 1, increment
-    end
+  defp split_words(text) do
+    Regex.scan(%r/[\p{L}0-9]+/i , String.downcase(text))
+  end
+
+  defp count_words(list) do
+    list |>
+    Enum.reduce(HashDict.new, fn [word], acc -> HashDict.update acc, word, 1, &(1 + &1) end)
   end
 
 end
