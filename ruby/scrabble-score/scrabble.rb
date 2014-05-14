cce1b57cd241d0277eb83eb4460de0438cbceb34
Alexander <alxndr+github@gmail.com>
2014-05-14 00:48:46 -0700

    ruby: scrabble-score: extract method for reduce
    and remove the implicit nil-handling of word.to_s

--- ruby/scrabble-score/scrabble.rb
+++ ruby/scrabble-score/scrabble.rb
@@ -1,20 +1,26 @@
 class Scrabble
 
-  VALUES = {
+  LETTER_VALUES = {
     A: 1, B: 3, C: 3, D: 2, E: 1, F: 4, G: 2, H: 4, I: 1, J: 8, K: 5, L: 1, M: 3,
     N: 1, O: 1, P: 3, Q:10, R: 1, S: 1, T: 1, U: 1, V: 4, W: 4, X: 8, Y: 4, Z:10
   }
 
   def initialize(word)
-    @word = word.to_s.strip.upcase
+    @word = (word || '').strip.upcase
   end
 
   def score
-    @word.each_char.reduce(0) { |memo, obj| memo += VALUES[obj.to_sym] }
+    @score ||= @word.each_char.reduce(0, &method(:sum_letter_values))
   end
 
   def self.score(word)
     self.new(word).score
+  end
+
+  private
+
+  def sum_letter_values(points, letter)
+    points += LETTER_VALUES[letter.to_sym]
   end
 
 end
