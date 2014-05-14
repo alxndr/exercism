ecdaf87c9d850cf17525a7057da5f940cd5b8827
Alexander <alxndr+github@gmail.com>
2014-05-13 22:09:14 -0700

    ruby: phone number: attr_reader & other trimming

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,14 +1,10 @@
 class PhoneNumber
 
-  def initialize(input)
-    number = letters_and_numbers(input)
-    number = maybe_trim_leading_one(number)
-    number = '0000000000' if still_invalid(number)
-    @number = number
-  end
+  attr_reader :number
 
-  def number
-    @number
+  def initialize(input)
+    @number = maybe_trim_leading_one(remove_invalid_characters(input))
+    @number = '0000000000' unless okay? @number
   end
 
   def to_s
@@ -16,20 +12,20 @@ class PhoneNumber
   end
 
   def area_code
-    @number[0,3]
+    number[0,3]
   end
 
   def exchange_number
-    @number[3,3]
+    number[3,3]
   end
 
   def local_number
-    @number[6,4]
+    number[6,4]
   end
 
   private
 
-  def letters_and_numbers(input)
+  def remove_invalid_characters(input)
     input.gsub(/[^a-z\d]/, '')
   end
 
@@ -41,8 +37,8 @@ class PhoneNumber
     end
   end
 
-  def still_invalid(num)
-    num.length < 10 || num.length === 11 || num.match(/[a-z]/)
+  def okay?(num)
+    num.length === 10 && !num.match(/[a-z]/)
   end
 
 end
