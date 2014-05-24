c40f59811bd53cba50201114a7c3b6e7197cc661
Alexander <alxndr+github@gmail.com>
2014-05-24 14:38:27 -0700

    elixir: list-ops: tidying

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -1,9 +1,7 @@
 defmodule ListOps do
 
   @spec count(list) :: non_neg_integer
-  def count(list) do
-    reduce list, 0, fn (_, acc) -> acc + 1 end
-  end
+  def count(list), do: reduce(list, 0, fn (_, acc) -> acc + 1 end)
 
   @spec reverse(list) :: list
   def reverse(list), do: _reverse(list, [])
@@ -15,32 +13,26 @@ defmodule ListOps do
   def map([ head | tail ], f), do: [ f.(head) | map(tail, f) ]
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter([], _f),            do: []
-  def filter([ head | tail ], f) do
-    if f.(head) do
-      [ head | filter(tail, f) ]
+  def filter([], _function),            do: []
+  def filter([ head | tail ], function) do
+    if function.(head) do
+      [ head | filter(tail, function) ]
     else
-      filter(tail, f)
+      filter(tail, function)
     end
   end
 
   @type acc :: any
   @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
-  def reduce([], acc, _f),        do: acc
-  def reduce([head|tail], acc, function) do
-    reduce tail, function.(head, acc), function
-  end
+  def reduce([], acc, _function),         do: acc
+  def reduce([head|tail], acc, function), do: reduce(tail, function.(head, acc), function)
 
   @spec append(list, list) :: list
   def append(list, list2),      do: _append(reverse(list), list2)
-  defp _append([], acc),       do: acc
-  defp _append([ h | t ], acc) do
-    _append(t, [ h | acc ])
-  end
+  defp _append([], acc),        do: acc
+  defp _append([ h | t ], acc), do: _append(t, [ h | acc ])
 
   @spec concat([[any]]) :: [any]
-  def concat(list) do
-    reduce list, [], &(append &2, &1)
-  end
+  def concat(list), do: reduce(list, [], &(append &2, &1))
 
 end
