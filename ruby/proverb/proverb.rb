c14a9c2d3f4b3922d47934e92105fc64a5d5ad01
Alexander <alxndr+github@gmail.com>
2014-06-09 21:29:58 -0700

    ruby: proverb: clean up after myself

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -2,22 +2,25 @@ class Proverb
 
   def initialize(*args, **opts)
     opts ||= {}
-    @proverb_lines = generate_lines(args, opts[:qualifier])
+    @proverb_lines = generate_lines(args, opts[:qualifier] || '')
   end
 
   def to_s
-    @proverb_lines.join "\n"
+    @proverb_lines.join("\n")
   end
 
   private
 
-  def generate_lines(nouns, qualifier)
-    noun_pairs = nouns.zip(nouns[1..-1])[0..-2] # [[nail, shoe], [shoe, horse], ..., [battle, kingdom]
-    puts noun_pairs.inspect
-    qualifier = qualifier ? qualifier + ' ' : ''
+  def generate_lines(nouns, qualifier='')
+    qualifier += ' ' unless qualifier.empty?
+    noun_pairs = list_to_pairs(nouns)
 
-    lines = noun_pairs.map { |first, second| "For want of a #{first} the #{second} was lost." }
+    lines = noun_pairs.map { |want, thing| "For want of a #{want} the #{thing} was lost." }
     lines.push "And all for the want of a #{qualifier}#{nouns.first}."
+  end
+
+  def list_to_pairs(list)
+    list.zip(list[1..-1])[0..-2]
   end
 
 end
