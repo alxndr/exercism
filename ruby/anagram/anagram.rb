77e0131cac271e2f2ba28c19925dbecb2aebc81c
Alexander <alxndr+github@gmail.com>
2014-06-09 20:57:12 -0700

    ruby: anagram: trim public interface
    and split apart AnagrammableWord#unique_anagram_of?

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -5,7 +5,7 @@ class Anagram
   end
 
   def match(words_to_check)
-    words_to_check.select { |word| @original_word.unique_anagram_of? word.downcase }
+    @original_word.find_anagrams(words_to_check)
   end
 
 end
@@ -15,19 +15,34 @@ class AnagrammableWord
   module StringAlphabetizer
     refine String do
       def alphabetize
-        self.each_char.sort.join
+        each_char.sort.join
       end
     end
   end
   using StringAlphabetizer
 
   def initialize(word)
-    @reference = word.downcase
-    @alphabetized_reference = @reference.alphabetize
+    @reference_word = word.downcase
+    @alphabetized_reference_word = @reference_word.alphabetize
   end
 
+  def find_anagrams(words)
+    words.select { |word| unique_anagram_of? word }
+  end
+
+  private
+
   def unique_anagram_of?(check_word)
-    check_word != @reference && check_word.alphabetize === @alphabetized_reference
+    check_word = check_word.downcase
+    unique_word?(check_word) && letters_match?(check_word)
+  end
+
+  def unique_word?(check_word)
+    check_word != @reference_word
+  end
+
+  def letters_match?(check_word)
+    check_word.alphabetize === @alphabetized_reference_word
   end
 
 end
