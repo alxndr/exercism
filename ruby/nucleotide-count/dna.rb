b2d2ad0e98ef0cfde43e8e346393ec08f2ed5d3d
Alexander <alxndr+github@gmail.com>
2014-06-14 23:13:34 -0700

    ruby: nucleotide-count: name census variable

--- ruby/nucleotide-count/dna.rb
+++ ruby/nucleotide-count/dna.rb
@@ -1,15 +1,11 @@
 class DNA
 
   def initialize(sequence)
-    @census = sequence.split('').each_with_object({
-      'A' => 0,
-      'T' => 0,
-      'C' => 0,
-      'G' => 0
-    }) do |letter, census|
+    sequence_census = { 'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
+    @census = sequence.split('').each_with_object(sequence_census) { |letter, census|
       raise ArgumentError unless census.has_key? letter
       census[letter] += 1
-    end
+    }
   end
 
   def count(nucleotide)
