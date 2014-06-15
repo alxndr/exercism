3f457d3f5be90302270b441e90c772b72aa105db
Alexander <alxndr+github@gmail.com>
2014-06-14 23:58:12 -0700

    js: hamming: shuffling things

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,18 +1,18 @@
 function compute(strand1, strand2) {
-  var strand1_a = strand1.split('');
-  var strand2_a = strand2.split('');
-
-  var difference = trim_when_smaller(strand1_a, strand2_a.length).reduce(count_differences, 0);
+  function trim_when_smaller(array, length) {
+    return array.splice(0, length);
+  }
   function count_differences(count, nucleotide, index) {
     if (nucleotide === strand2_a[index]) {
       return count;
     }
     return count + 1;
   }
-  function trim_when_smaller(array, length) {
-    return array.splice(0, length);
-  }
-  return difference;
+
+  var strand1_a = strand1.split('');
+  var strand2_a = strand2.split('');
+
+  return trim_when_smaller(strand1_a, strand2_a.length).reduce(count_differences, 0);
 }
 
 if (module) {
