bdfe548bc5402719a229c19e6ed14bd4b6e9ecec
Alexander <alxndr+github@gmail.com>
2014-06-14 23:01:04 -0700

    ruby: etl: can use Enumerable#each_with_object instead of #reduce and save a line

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -2,10 +2,9 @@ class ETL
 
   def self.transform(old_schema)
     old_schema.reduce({}) { |memo, (point_value, letters)|
-      memo.merge(letters.reduce({}) { |hash, letter|
+      memo.merge letters.each_with_object({}) { |letter, hash|
         hash[letter.downcase] = point_value
-        hash
-      })
+      }
     }
   end
 
