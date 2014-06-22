e5e6abb541fe81a01fa55223b4396e07d84b3784
Alexander <alxndr+github@gmail.com>
2014-06-21 20:22:10 -0700

    ruby: gigasecond: little refactoring

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,12 +1,21 @@
 class Gigasecond
 
-  BILLION = 10**9
+  SECONDS_PER_GS = 10**9
 
   attr_reader :date
 
   def initialize(start_date)
-    @starting_time = start_date.to_time
-    @date = (@starting_time + BILLION).to_date
+    @start_date = start_date
+  end
+
+  def date
+    @gigasec_from_start_time ||= calculate_gigasec_from_start_time
+  end
+
+  private
+
+  def calculate_gigasec_from_start_time
+    (@start_date.to_time + SECONDS_PER_GS).to_date
   end
 
 end
