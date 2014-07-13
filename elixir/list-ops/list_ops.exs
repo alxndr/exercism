078524a073a557cae5ca7de4655d8334293360e9
Alexander <alxndr+github@gmail.com>
2014-07-12 23:23:02 -0700

    elixir: list-ops: massaging

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -2,8 +2,8 @@ defmodule ListOps do
 
   @type acc :: any
   @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
-  def reduce([], acc, _function),         do: acc
-  def reduce([head|tail], acc, function), do: reduce(tail, function.(head, acc), function)
+  def reduce([], acc, _fxn),   do: acc
+  def reduce([h|t], acc, fxn), do: reduce(t, fxn.(h, acc), fxn)
 
   @spec count(list) :: non_neg_integer
   def count(list), do: reduce(list, 0, fn (_, acc) -> acc + 1 end)
@@ -18,9 +18,11 @@ defmodule ListOps do
   def concat(list), do: reduce(list, [], &(append &2, &1))
 
   @spec map(list, (any -> any)) :: list
-  def map(list, function), do: reduce(reverse(list), [], &([ function.(&1) | &2 ]))
+  def map(list, fxn), do: reduce(reverse(list), [], &([ fxn.(&1) | &2 ]))
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(list, function), do: reduce(reverse(list), [], &(if function.(&1), do: [ &1 | &2 ], else: &2))
+  def filter(list, fxn) do
+    reduce(reverse(list), [], &(if fxn.(&1), do: [ &1 | &2 ], else: &2))
+  end
 
 end
