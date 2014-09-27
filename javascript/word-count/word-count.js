470b7c4a9cbff51a448c110aa7c303843cd61df4
Alexander <alxndr+github@gmail.com>
2014-09-27 12:11:04 -0700

    js: word-count: pass new unicode char test

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -7,8 +7,10 @@ function count_unique_elements(accumulator, element) {
   return accumulator;
 }
 
+var REGEX_PUNCTUATION = /[\u00A1\u00BF\u2000-\u206F\u2E00-\u2E7F\\'!"#\$%&\(\)\*\+,\-\.\/:;<=>\?@\[\]\^_`\{\|\}~]/g;
+
 function strip_punctuation(str) {
-  return str.toLowerCase().replace(/[^a-z0-9 ]/g, '');
+  return str.replace(REGEX_PUNCTUATION, '').toLowerCase();
 }
 
 function word_analyzer(text) {
