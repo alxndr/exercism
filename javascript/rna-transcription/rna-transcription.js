045d2acc8b1e5518b0956381a01addba139ecaa5
Alexander <alxndr+github@gmail.com>
2014-10-06 20:21:22 -0700

    js: rna-transcription: camelCase

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,17 +1,17 @@
-var dna_to_rna_mapping = {
+var dnaToRnaMapping = {
   C: 'G',
   G: 'C',
   A: 'U',
   T: 'A'
 };
-function dna_nucleotide_to_rna(dna_nucleotide) {
-  return dna_to_rna_mapping[dna_nucleotide];
+function dnaNucleotideToRna(dnaNucleotide) {
+  return dnaToRnaMapping[dnaNucleotide];
 }
 
-function dna_nucleotides_to_rna(dna_nucleotides) {
-  return dna_nucleotides.replace(/./g, dna_nucleotide_to_rna);
+function dnaNucleotidesToRna(dnaNucleotides) {
+  return dnaNucleotides.replace(/./g, dnaNucleotideToRna);
 }
 
 if (module) {
-  module.exports = dna_nucleotides_to_rna;
+  module.exports = dnaNucleotidesToRna;
 }
