d14b0b07f44ddc0a8ee5e78810537cb10a6400a4
Alexander <alxndr+github@gmail.com>
2015-01-20 22:11:20 -0800

    ruby: rna: base it fully off the MAPPINGS constant

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -5,7 +5,7 @@ class Complement
     RNA: { C: :G, G: :C, U: :A, A: :T },
   }
 
-  [:DNA, :RNA].each do |molecule|
+  MAPPINGS.keys.each do |molecule|
     self.define_singleton_method("of_#{molecule.downcase}") do |nbases|
       nbases.split('').map { |nbase| MAPPINGS[molecule][nbase.to_sym].to_s }.join('')
     end
