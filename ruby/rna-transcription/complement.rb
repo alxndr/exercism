9ca89f836b48d6e1ee30e80a013412ca1c9e1ff4
Alexander <alxndr+github@gmail.com>
2015-01-30 18:40:25 -0800

    ruby: rna: define a #molecules
    kinda wish it could be private though

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -6,12 +6,18 @@ DNA_TO_RNA = {
 }
 
 class Complement
-  {
-    DNA: DNA_TO_RNA,
-    RNA: DNA_TO_RNA.invert,
-  }.each do |molecule_symbol, molecule_mapping|
+
+  def self.molecules
+    {
+      DNA: DNA_TO_RNA,
+      RNA: DNA_TO_RNA.invert,
+    }
+  end
+
+  molecules.each do |molecule_symbol, molecule_mapping|
     self.define_singleton_method("of_#{molecule_symbol.downcase}") do |nbases|
       nbases.each_char.map { |nbase| molecule_mapping[nbase] }.join
     end
   end
+
 end
