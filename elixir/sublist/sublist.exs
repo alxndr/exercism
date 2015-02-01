5cd445e0bfaffdd682a16d388001b0f24d3c6f1b
Alexander <alxndr+github@gmail.com>
2015-01-31 20:16:22 -0800

    elixir: sublist: rename things

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -7,22 +7,28 @@ defmodule Sublist do
   iex> SubList.compare([], [])
   :equal
   """
+  def compare(l, l), do: :equal
+  def compare([], _), do: :sublist
   def compare(first, second) do
     case { length(first), length(second) } do
-      { first_length, second_length } when first_length < second_length -> if sublist?(first, second, first_length, second_length), do: :sublist, else: :unequal
-      { first_length, second_length } when first_length > second_length -> if sublist?(second, first, second_length, first_length), do: :superlist, else: :unequal
-      _ -> if first == second, do: :equal, else: :unequal
+      { first_length, second_length } when first_length < second_length -> if sublist?(first, second), do: :sublist, else: :unequal
+      { first_length, second_length } when first_length > second_length -> if sublist?(second, first), do: :superlist, else: :unequal
+      _ -> :unequal
     end
   end
 
-  defp sublist?([], _, _, _), do: true
-  defp sublist?(_, _, first_length, second_length) when first_length > second_length, do: false
-  defp sublist?(first, second=[_|second_tail], first_length, second_length) do
-    if _sublist?(first, second), do: true, else: sublist?(first, second_tail, first_length, second_length - 1)
+  defp sublist?([], _), do: true
+  defp sublist?(first, second) do
+    _sublist_with_lengths?(first, second, length(first), length(second))
   end
 
-  defp _sublist?([], _), do: true
-  defp _sublist?([a|first_tail], [a|second_tail]), do: _sublist?(first_tail, second_tail)
-  defp _sublist?(_, _), do: false
+  defp _sublist_with_lengths?(_, _, first_length, second_length) when first_length > second_length, do: false
+  defp _sublist_with_lengths?(first, second=[_|second_tail], first_length, second_length) do
+    if _sublist_without_lengths?(first, second), do: true, else: _sublist_with_lengths?(first, second_tail, first_length, second_length - 1)
+  end
+
+  defp _sublist_without_lengths?([], _), do: true
+  defp _sublist_without_lengths?([a|first_tail], [a|second_tail]), do: _sublist_without_lengths?(first_tail, second_tail)
+  defp _sublist_without_lengths?(_, _), do: false
 
 end
