46231b4d31bd3a3c84cdd949fdd56742cc30f5ad
Alexander <alxndr+github@gmail.com>
2015-02-01 14:00:19 -0800

    elixir: point-mutations: using Stream a little
    ...in basic benchmarking (see test) it's comparable to prior
    implementation

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -8,16 +8,17 @@ defmodule DNA do
   iex> DNA.hamming_distance('AAGTCATA', 'TAGCGATC')
   4
   """
-  @spec hamming_distance([char], [char]) :: non_neg_integer
+  @spec hamming_distance([char], [char]) :: non_neg_integer | nil
   def hamming_distance(strand1, strand2) when length(strand1) != length(strand2), do: nil
   def hamming_distance(strand1, strand2) do
     strand1
     |> Stream.zip(strand2)
-    |> Enum.count(&different_chars?/1)
+    |> Stream.reject(&identical_chars?/1)
+    |> Enum.count
   end
 
-  @spec different_chars?({char, char}) :: boolean
-  defp different_chars?({char, char}), do: false
-  defp different_chars?({_char1, _char2}), do: true
+  @spec identical_chars?({char, char}) :: boolean
+  defp identical_chars?({char1, char2}) when char1 === char2, do: true
+  defp identical_chars?({_, _}), do: false
 
 end
