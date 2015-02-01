3dec29242f3d61b5e62fd1f93f85801c844b6132
Alexander <alxndr+github@gmail.com>
2015-02-01 00:55:50 -0800

    elixir: anagram: working wth Enum.filter/2

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -1,9 +1,27 @@
 defmodule Anagram do
+
   @doc """
   Returns all candidates that are anagrams of, but not equal to, 'base'.
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-  
+    base_downcased = base
+                      |> String.downcase
+    base_alphabetized = base_downcased
+                        |> alphabetize
+    candidates
+    |> Enum.filter(fn (candidate) ->
+      candidate_downcased = candidate
+                            |> String.downcase
+      base_alphabetized == alphabetize(candidate_downcased) && candidate_downcased != base_downcased
+    end)
   end
+
+  @spec alphabetize(String.t) :: String.t
+  defp alphabetize(word) do
+    word
+    |> String.to_char_list
+    |> Enum.sort
+  end
+
 end
