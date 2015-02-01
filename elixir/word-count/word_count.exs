cfd8e6589b431d4386e86e97c87cceaaa7c86b26
Alexander <alxndr+github@gmail.com>
2015-01-31 17:13:49 -0800

    elixir: word-count: whitespace

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -1,15 +1,18 @@
 defmodule Words do
 
   def count(text) do
-    text |> split_words |> count_words
+    text
+    |> split_words
+    |> count_words
   end
 
   defp split_words(text) do
-    Regex.scan(~r/[\p{L}0-9]+/i , String.downcase(text))
+    Regex.scan ~r/[\p{L}0-9]+/i, String.downcase(text)
   end
 
   defp count_words(list) do
-    list |> Enum.reduce(HashDict.new, fn [word], acc -> HashDict.update acc, word, 1, &(1 + &1) end)
+    list
+    |> Enum.reduce(HashDict.new, fn [word], acc -> HashDict.update acc, word, 1, &(1 + &1) end)
   end
 
 end
