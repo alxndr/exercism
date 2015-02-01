e132188e929c4d2a38850ecb3084e85fc2497843
Alexander <alxndr+github@gmail.com>
2015-02-01 13:25:56 -0800

    elixir: point-mutations: added and solved
    zip and count

---
+++ elixir/point-mutations/point_mutations.exs
@@ -0,0 +1,23 @@
+defmodule DNA do
+
+  @doc """
+  Returns number of differences between two strands of DNA, known as the Hamming Distance.
+
+  ## Examples
+
+  iex> DNA.hamming_distance('AAGTCATA', 'TAGCGATC')
+  4
+  """
+  @spec hamming_distance([char], [char]) :: non_neg_integer
+  def hamming_distance(strand1, strand2) when length(strand1) != length(strand2), do: nil
+  def hamming_distance(strand1, strand2) do
+    strand1
+    |> Stream.zip(strand2)
+    |> Enum.count(&different_chars?/1)
+  end
+
+  @spec different_chars?({char, char}) :: boolean
+  defp different_chars?({ char, char }), do: false
+  defp different_chars?({ _char1, _char2 }), do: true
+
+end
