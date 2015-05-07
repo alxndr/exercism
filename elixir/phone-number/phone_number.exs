277a03a5b1f0b9807d2ef8e81c60cb2e64307b35
Alexander <alxndr+github@gmail.com>
2015-05-06 22:20:15 -0700

    elixir: phone_number: reorganize

--- elixir/phone-number/phone_number.exs
+++ elixir/phone-number/phone_number.exs
@@ -23,24 +23,6 @@ defmodule Phone do
     |> maybe_trim
   end
 
-  @spec sanitize_number(String.t) :: String.t
-  defp sanitize_number(numerical_input) do
-    numerical_input
-    |> String.replace(~r{\D}, "")
-  end
-
-  @spec maybe_trim(String.t) :: String.t
-  defp maybe_trim(numerical_string) do
-    cond do
-      matches = Regex.named_captures(~r/^1(?<remainder>\d{10}+)$/, numerical_string) ->
-        matches["remainder"]
-      Regex.named_captures(~r/^1(?<remainder>\d{9}+)$/, numerical_string) ->
-        numerical_string
-      true ->
-        "0000000000"
-    end
-  end
-
   @doc """
   Extract the area code from a phone number
 
@@ -61,23 +43,8 @@ defmodule Phone do
   @spec area_code(String.t) :: String.t
   def area_code(raw) do
     raw
-    |> sanitize_number
-    |> trim_extra_leading_one
+    |> cleanup
     |> String.slice 0, 3
-  end
-
-  @spec trim_extra_leading_one(String.t) :: String.t
-  defp trim_extra_leading_one(numerical_string) do
-    if has_extra_leading_one?(numerical_string) do
-      Regex.named_captures(~r/^1(?<remainder>\d{10}+)$/, numerical_string)["remainder"]
-    else
-      numerical_string
-    end
-  end
-
-  @spec has_extra_leading_one?(String.t) :: boolean
-  defp has_extra_leading_one?(numerical_string) do
-    String.length(numerical_string) == 11 && String.starts_with?(numerical_string, "1")
   end
 
   @doc """
@@ -100,12 +67,51 @@ defmodule Phone do
   @spec pretty(String.t) :: String.t
   def pretty(raw) do
     raw
-    |> sanitize_number
-    |> trim_extra_leading_one
+    |> cleanup
     |> (fn (digits) ->
       { area_code, other_stuff } = String.split_at(digits, 3)
       { first_three, last_four } = String.split_at(other_stuff, 3)
       "(#{area_code}) #{first_three}-#{last_four}"
     end).()
   end
+
+  @spec cleanup(String.t) :: String.t
+  defp cleanup(raw_phone_number) do
+    raw_phone_number
+    |> sanitize_number
+    |> trim_extra_leading_one
+  end
+
+  @spec sanitize_number(String.t) :: String.t
+  defp sanitize_number(numerical_input) do
+    numerical_input
+    |> String.replace(~r{\D}, "")
+  end
+
+  @spec maybe_trim(String.t) :: String.t
+  defp maybe_trim(numerical_string) do
+    cond do
+      matches = Regex.named_captures(~r/^1(?<remainder>\d{10}+)$/, numerical_string) ->
+        matches["remainder"]
+      Regex.named_captures(~r/^1(?<remainder>\d{9}+)$/, numerical_string) ->
+        numerical_string
+      true ->
+        "0000000000"
+    end
+  end
+
+  @spec trim_extra_leading_one(String.t) :: String.t
+  defp trim_extra_leading_one(numerical_string) do
+    if has_extra_leading_one?(numerical_string) do
+      Regex.named_captures(~r/^1(?<remainder>\d{10}+)$/, numerical_string)["remainder"]
+    else
+      numerical_string
+    end
+  end
+
+  @spec has_extra_leading_one?(String.t) :: boolean
+  defp has_extra_leading_one?(numerical_string) do
+    String.length(numerical_string) == 11 && String.starts_with?(numerical_string, "1")
+  end
+
 end
