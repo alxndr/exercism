f1aef1fa842fb5cb393186329beae3851afef1b6
Alexander <alxndr+github@gmail.com>
2015-05-10 15:26:31 -0700

    elixir: space_age: use `use`

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -1,5 +1,12 @@
 defmodule SpaceAgeMacros do
-  defmacro def_age_on(planet, opts \\ []) do
+
+  defmacro __using__(_options) do
+    quote do
+      import unquote(__MODULE__)
+    end
+  end
+
+  defmacro def_age_on(planet, opts) do
     quote do
       @doc """
       Return the number of years a person that has lived for 'seconds' seconds is
@@ -14,20 +21,20 @@ defmodule SpaceAgeMacros do
 end
 
 defmodule SpaceAge do
-  require SpaceAgeMacros
+  use SpaceAgeMacros
 
   @type planet :: :mercury | :venus | :earth | :mars | :jupiter
                 | :saturn | :neptune | :uranus
 
   @earth_orbital_period_in_sec 60*60*24*365.25
 
-  SpaceAgeMacros.def_age_on :mercury, multiplier: 0.2408467
-  SpaceAgeMacros.def_age_on :venus,   multiplier: 0.6151973
-  SpaceAgeMacros.def_age_on :earth,   multiplier: 1
-  SpaceAgeMacros.def_age_on :mars,    multiplier: 1.8808158
-  SpaceAgeMacros.def_age_on :jupiter, multiplier: 11.862815
-  SpaceAgeMacros.def_age_on :saturn,  multiplier: 29.447498
-  SpaceAgeMacros.def_age_on :uranus,  multiplier: 84.016846
-  SpaceAgeMacros.def_age_on :neptune, multiplier: 164.79132
+  def_age_on :mercury, multiplier: 0.2408467
+  def_age_on :venus,   multiplier: 0.6151973
+  def_age_on :earth,   multiplier: 1
+  def_age_on :mars,    multiplier: 1.8808158
+  def_age_on :jupiter, multiplier: 11.862815
+  def_age_on :saturn,  multiplier: 29.447498
+  def_age_on :uranus,  multiplier: 84.016846
+  def_age_on :neptune, multiplier: 164.79132
 
 end
