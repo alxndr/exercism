01e80f4ff9f730da0d93386879eadc3ecc255001
Alexander <alxndr+github@gmail.com>
2015-05-11 07:39:01 -0700

    elixir: space_age: excise the macro

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -1,39 +1,28 @@
-defmodule SpaceAgeMacros do
-
-  defmacro __using__(_options) do
-    quote do
-      import unquote(__MODULE__)
-    end
-  end
-
-  defmacro def_ages_on(planets) do
-    Enum.map(planets, fn ({planet, multiplier}) ->
-      quote do
-        def age_on(unquote(planet), seconds) do
-          seconds / (@earth_orbital_period_in_sec * unquote(multiplier))
-        end
-      end
-    end)
-  end
-end
-
 defmodule SpaceAge do
-  use SpaceAgeMacros
 
   @type planet :: :mercury | :venus | :earth | :mars | :jupiter
                 | :saturn | :neptune | :uranus
 
   @earth_orbital_period_in_sec 60*60*24*365.25
 
-  def_ages_on(
-    mercury: 0.2408467,
-    venus: 0.6151973,
-    earth: 1,
-    mars: 1.8808158,
-    jupiter: 11.862815,
-    saturn: 29.447498,
-    uranus: 84.016846,
-    neptune: 164.79132
-  )
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(:earth, seconds) do
+    seconds / @earth_orbital_period_in_sec
+  end
+  def age_on(planet, seconds) do
+    seconds / (@earth_orbital_period_in_sec * planet_orbit_relative_to_earth(planet))
+  end
+
+  defp planet_orbit_relative_to_earth(:mercury), do: 0.2408467
+  defp planet_orbit_relative_to_earth(:venus),   do: 0.6151973
+  defp planet_orbit_relative_to_earth(:mars),    do: 1.8808158
+  defp planet_orbit_relative_to_earth(:jupiter), do: 11.862815
+  defp planet_orbit_relative_to_earth(:saturn),  do: 29.447498
+  defp planet_orbit_relative_to_earth(:uranus),  do: 84.016846
+  defp planet_orbit_relative_to_earth(:neptune), do: 164.79132
 
 end
