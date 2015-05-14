b79d35e1e7aa462d2f4e5c87a52db78612bdaf55
Alexander <alxndr+github@gmail.com>
2015-05-13 23:52:06 -0700

    elixir: triangle: earlier guard clauses cover the isosceles ones

--- elixir/triangle/triangle.exs
+++ elixir/triangle/triangle.exs
@@ -11,9 +11,9 @@ defmodule Triangle do
   def kind(a, b, c) when a <= 0     or b <= 0     or c <= 0,     do: @error_negative
   def kind(a, b, c) when a + b <= c or a + c <= b or b + c <= a, do: @error_equality
   def kind(n, n, n), do: {:ok, :equilateral}
-  def kind(odd, even, even) when even + even > odd, do: {:ok, :isosceles}
-  def kind(even, odd, even) when even + even > odd, do: {:ok, :isosceles}
-  def kind(even, even, odd) when even + even > odd, do: {:ok, :isosceles}
+  def kind(_odd, even, even), do: {:ok, :isosceles}
+  def kind(even, _odd, even), do: {:ok, :isosceles}
+  def kind(even, even, _odd), do: {:ok, :isosceles}
   def kind(_, _, _), do: {:ok, :scalene}
 
 end
