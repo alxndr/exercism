cb076dd6a92f1ea5550644ede2cd4d4c19a59967
Alexander <alxndr+github@gmail.com>
2015-05-13 21:16:37 -0700

    elixir: space-age: define earth like the others

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -4,21 +4,18 @@ defmodule SpaceAge do
                 | :saturn | :neptune | :uranus
 
   @earth_orbital_period_in_sec 60*60*24*365.25
-
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet'.
   """
   @spec age_on(planet, pos_integer) :: float
-  def age_on(:earth, seconds) do
-    seconds / @earth_orbital_period_in_sec
-  end
   def age_on(planet, seconds) do
     seconds / (@earth_orbital_period_in_sec * planet_orbit_relative_to_earth(planet))
   end
 
   defp planet_orbit_relative_to_earth(:mercury), do: 0.2408467
   defp planet_orbit_relative_to_earth(:venus),   do: 0.6151973
+  defp planet_orbit_relative_to_earth(:earth),   do: 1
   defp planet_orbit_relative_to_earth(:mars),    do: 1.8808158
   defp planet_orbit_relative_to_earth(:jupiter), do: 11.862815
   defp planet_orbit_relative_to_earth(:saturn),  do: 29.447498
