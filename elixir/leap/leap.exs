eaebaa8f34d02dce910f08bd6a4ca3fc9dcecfb0
Alexander <alxndr+github@gmail.com>
2015-05-13 20:26:10 -0700

    elixir: leap: eliminate some unnecessary math

--- elixir/leap/leap.exs
+++ elixir/leap/leap.exs
@@ -9,14 +9,8 @@ defmodule Year do
       except every year that is evenly divisible by 400.
   """
   @spec leap_year?(non_neg_integer) :: boolean
-  def leap_year?(y)
-  when rem(y, 4) == 0
-  and rem(y, 100) == 0
-  and rem(y, 400) == 0, do: true
-  def leap_year?(y)
-  when rem(y, 4) == 0
-  and rem(y, 100) == 0, do: false
-  def leap_year?(y)
-  when rem(y, 4) == 0, do: true
-  def leap_year?(_year), do: false
+  def leap_year?(y) when rem(y, 400) == 0, do: true
+  def leap_year?(y) when rem(y, 100) == 0, do: false
+  def leap_year?(y) when rem(y, 4) == 0,   do: true
+  def leap_year?(_year),                   do: false
 end
