06c732af0fcb0f5b5a9315dc371479df80bc4e79
Alexander <alxndr+github@gmail.com>
2015-05-13 23:51:07 -0700

    elixir: triangle: little refactor

--- elixir/triangle/triangle.exs
+++ elixir/triangle/triangle.exs
@@ -8,10 +8,8 @@ defmodule Triangle do
   Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
   """
   @spec kind(number, number, number) :: { :ok, kind } | { :error, String.t }
-  def kind(n, _, _) when n <= 0, do: @error_negative
-  def kind(_, n, _) when n <= 0, do: @error_negative
-  def kind(_, _, n) when n <= 0, do: @error_negative
-  def kind(a, b, c) when a >= b + c or b >= a + c or c >= a + b, do: @error_equality
+  def kind(a, b, c) when a <= 0     or b <= 0     or c <= 0,     do: @error_negative
+  def kind(a, b, c) when a + b <= c or a + c <= b or b + c <= a, do: @error_equality
   def kind(n, n, n), do: {:ok, :equilateral}
   def kind(odd, even, even) when even + even > odd, do: {:ok, :isosceles}
   def kind(even, odd, even) when even + even > odd, do: {:ok, :isosceles}
