516c84ca1b31275bdd7b25a4362d8ed472d2e9c5
Alexander <alxndr+github@gmail.com>
2015-05-16 18:01:08 -0700

    elixir: adding prime-factors

---
+++ elixir/prime-factors/prime_factors.exs
@@ -0,0 +1,14 @@
+defmodule PrimeFactors do
+  @doc """
+  Compute the prime factors for 'number'.
+
+  The prime factors are prime numbers that when multiplied give the desired
+  number.
+
+  The prime factors of 'number' will be ordered lowest to highest. 
+  """
+  @spec factors_for(pos_integer) :: [pos_integer]
+  def factors_for(number) do
+
+  end
+end
