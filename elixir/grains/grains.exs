e3e733179b2b93d98ffc18df1b4be3a921c2fb30
Alexander <alxndr+github@gmail.com>
2015-05-30 15:42:12 -0700

    elixir: grains: extract a Math module

--- elixir/grains/grains.exs
+++ elixir/grains/grains.exs
@@ -3,10 +3,7 @@ defmodule Grains do
   Calculate two to the power of the input minus one.
   """
   @spec square(pos_integer) :: pos_integer
-  def square(1), do: 1
-  def square(n) do
-    2 * square(n - 1)
-  end
+  def square(n), do: Math.pow(2, n - 1)
 
   @doc """
   Adds square of each number from 1 to 64.
@@ -15,5 +12,14 @@ defmodule Grains do
   def total do
     1..64
     |> Enum.reduce(0, &(&2 + square(&1)))
+  end
+
+end
+
+defmodule Math do
+  def pow(n, 0), do: 1
+  def pow(n, 1), do: n
+  def pow(n, exponent) do
+    n * pow(n, exponent-1)
   end
 end
