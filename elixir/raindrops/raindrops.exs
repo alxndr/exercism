3226a5943975c4dafa303d9e16f2b96a8505210a
Alexander <alxndr+github@gmail.com>
2015-06-06 16:04:01 -0700

    elixir: raindrops: working

--- elixir/raindrops/raindrops.exs
+++ elixir/raindrops/raindrops.exs
@@ -1,4 +1,13 @@
 defmodule Raindrops do
+
+  @factor_pairs [
+    "Pling": 3,
+    "Plang": 5,
+    "Plong": 7,
+  ]
+
+  @default ""
+
   @doc """
   Returns a string based on raindrop factors.
 
@@ -10,6 +19,18 @@ defmodule Raindrops do
   """
   @spec convert(pos_integer) :: String.t
   def convert(number) do
-
+    result =
+      @factor_pairs
+      |> Enum.reduce(@default, fn
+        ({term, n}, str) when number >= n and rem(number, n) == 0 ->
+          str <> to_string(term)
+        (_, str) ->
+          str
+      end)
+    if result == @default do
+      to_string number
+    else
+      result
+    end
   end
 end
