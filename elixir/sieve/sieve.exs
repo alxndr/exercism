ed7025aa7f4e6dbd2a92b1b71fff941524defeab
Alexander <alxndr+github@gmail.com>
2015-06-06 15:12:33 -0700

    elixir: sieve: working

--- elixir/sieve/sieve.exs
+++ elixir/sieve/sieve.exs
@@ -5,7 +5,20 @@ defmodule Sieve do
   """
   @spec primes_to(non_neg_integer) :: [non_neg_integer]
   def primes_to(limit) do
-
+    (2..limit)
+    |> Enum.to_list # TODO try to make lazy?
+    |> primes_in(0)
   end
 
+  defp primes_in(raw_sieve, index) when index >= length(raw_sieve), do: raw_sieve
+  defp primes_in(raw_sieve, index) do
+    factor = Enum.at(raw_sieve, index)
+    raw_sieve
+    |> Enum.reduce([], fn # could use Enum.reject
+      (n, sieve) when n == factor         -> sieve ++ [n]
+      (n, sieve) when rem(n, factor) == 0 -> sieve
+      (n, sieve)                          -> sieve ++ [n]
+    end)
+    |> primes_to(index + 1)
+  end
 end
