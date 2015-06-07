ee1a9ab67c403c1542e8c9c8e05da4962171cf1c
Alexander <alxndr+github@gmail.com>
2015-06-06 22:30:28 -0700

    elixir: atbash-cipher: working

--- elixir/atbash-cipher/atbash.exs
+++ elixir/atbash-cipher/atbash.exs
@@ -1,4 +1,5 @@
 defmodule Atbash do
+
   @doc """
   Encode a given plaintext to the corresponding ciphertext
 
@@ -9,6 +10,39 @@ defmodule Atbash do
   """
   @spec encode(String.t) :: String.t
   def encode(plaintext) do
-  
+    plaintext
+    |> String.graphemes
+    |> sanitize
+    |> Enum.reverse
+    |> encode([])
+    |> Enum.chunk(5, 5, [])
+    |> Enum.map(&Enum.join(&1, ""))
+    |> Enum.join(" ")
   end
+  defp encode([], encoded), do: encoded
+  defp encode([h|t], encoded) do
+    encode(t, [encoded_letter(h) | encoded])
+  end
+
+  @spec sanitize([String.t]) :: [String.t]
+  defp sanitize(letters) do
+    sanitize(letters, [])
+  end
+  defp sanitize([], sanitized_letters), do: sanitized_letters
+  defp sanitize([letter|letters], sanitized_letters) do
+    letter = String.downcase(letter)
+    cond do
+      Regex.match?(~r/^[0-9]$/, letter) ->
+        sanitize(letters, sanitized_letters ++ [letter])
+      Regex.match?(~r/^[a-z]$/, letter) ->
+        sanitize(letters, sanitized_letters ++ [letter])
+      true ->
+        sanitize(letters, sanitized_letters)
+    end
+  end
+
+  @spec encoded_letter(String.t) :: String.t
+  defp encoded_letter(num_str = <<n>>) when n >= 48 and n <= 57, do: num_str
+  defp encoded_letter(<<n>>) when n >= 97 and n <= 122, do: << 219 - n >>
+
 end
