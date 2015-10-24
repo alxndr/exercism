ae3714ba739775e810dcb8bb7e28979b78818c06
Alexander <alxndr+github@gmail.com>
2015-10-24 16:46:00 -0700

    js: bunch of spec updates

---
+++ javascript/leap/leap-test.spec.js
@@ -0,0 +1,42 @@
+var isLeapYear = require('./leap');
+
+describe('Leap year', function() {
+
+  it('is not very common', function() {
+    expect(isLeapYear(2015)).toBe(false);
+  });
+
+  xit('is introduced every 4 years to adjust about a day', function() {
+    expect(isLeapYear(2016)).toBe(true);
+  });
+
+  xit('is skipped every 100 years to remove an extra day', function() {
+    expect(isLeapYear(1900)).toBe(false);
+  });
+
+  xit('is reintroduced every 400 years to adjust another day', function() {
+    expect(isLeapYear(2000)).toBe(true);
+  });
+
+  // Feel free to enable the following tests to check some more examples
+  xdescribe('Additional example of a leap year that', function () {
+
+    it('is not a leap year', function () {
+      expect(isLeapYear(1978)).toBe(false);
+    });
+
+    it('is a common leap year', function () {
+      expect(isLeapYear(1992)).toBe(true);
+    });
+
+    it('is skipped every 100 years', function () {
+      expect(isLeapYear(2100)).toBe(false);
+    });
+
+    it('is reintroduced every 400 years', function () {
+      expect(isLeapYear(2400)).toBe(true);
+    });
+
+  });
+
+});
