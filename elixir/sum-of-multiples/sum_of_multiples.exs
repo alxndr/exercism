e0aa154dc96e07d8c32c88eaa8591a1213a1ff0f
Alexander <alxndr+github@gmail.com>
2015-10-24 19:40:15 -0700

    elixir: sum-of-multiples: working

--- elixir/sum-of-multiples/sum_of_multiples.exs
+++ elixir/sum-of-multiples/sum_of_multiples.exs
@@ -6,6 +6,21 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors \\ [3, 5]) do
-    
+    sum_to(2, limit, factors, 0)
+  end
+
+  defp sum_to(n, limit, _factors, sum) when n >= limit, do: sum
+  defp sum_to(n, limit, factors, sum) do
+    new_sum = if is_a_factor?(n, factors) do
+      sum + n
+    else
+      sum
+    end
+    sum_to(n + 1, limit, factors, new_sum)
+  end
+
+  defp is_a_factor?(n, factors) do
+    factors
+    |> Enum.any? &(rem(n, &1) == 0)
   end
 end
