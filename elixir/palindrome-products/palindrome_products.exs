eb3e68cb95a1e365b21ce5b8167b8f1c08c7d376
Alexander <alxndr+github@gmail.com>
2015-10-28 08:44:20 -0700

    palindrome_products WIP

--- elixir/palindrome-products/palindrome_products.exs
+++ elixir/palindrome-products/palindrome_products.exs
@@ -3,8 +3,14 @@ defmodule Palindromes do
   @doc """
   Generates all palindrome products from an optionally given min factor (or 1) to a given max factor.
   """
-  @spec generate(non_neg_integer, non_neg_integer) :: map() 
+  @spec generate(non_neg_integer, non_neg_integer) :: map()
   def generate(max_factor, min_factor \\ 1) do
-    
+    all_pairs
+    |> IO.inspect
+  end
+
+  defp all_pairs do
+    for a <- min_factor..max_factor,
+        b <- a..max_factor, do: [a, b]
   end
 end
