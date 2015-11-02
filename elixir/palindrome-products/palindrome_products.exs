b351354d0cf5f3004db84023d0e1e166e4640953
Alexander <alxndr+github@gmail.com>
2015-11-01 23:20:04 -0800

    elixir: palindrome-products: working

--- elixir/palindrome-products/palindrome_products.exs
+++ elixir/palindrome-products/palindrome_products.exs
@@ -5,12 +5,19 @@ defmodule Palindromes do
   """
   @spec generate(non_neg_integer, non_neg_integer) :: map()
   def generate(max_factor, min_factor \\ 1) do
-    all_pairs
-    |> IO.inspect
+    palindromes(max_factor, min_factor)
+    |> Enum.reduce(%{}, &add_to_map/2)
   end
 
-  defp all_pairs do
+  defp palindromes(max_factor, min_factor) do
     for a <- min_factor..max_factor,
-        b <- a..max_factor, do: [a, b]
+    b <- a..max_factor,
+    String.reverse("#{a * b}") == "#{a * b}",
+      do: {a * b, [a, b]}
+  end
+
+  defp add_to_map({product, pairs}, map) do
+    map
+    |> Map.put(product, Map.get(map, product, []) ++ [pairs])
   end
 end
